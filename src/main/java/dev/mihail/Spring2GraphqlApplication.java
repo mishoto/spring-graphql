835111eb521c6330c5950a943d5394defd8a1896
Mishoto <mihail.dev.io@gmail.com>
2021-11-02 17:24:37 +0200

    Initial Commit

---
+++ src/main/java/dev/mihail/Spring2GraphqlApplication.java
@@ -0,0 +1,13 @@
+package dev.mihail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2GraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2GraphqlApplication.class, args);
+	}
+
+}
