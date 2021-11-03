3234001cdfe64cf63cc931d28146b122fc2c9a5d
Mishoto <mihail.dev.io@gmail.com>
2021-11-03 18:11:19 +0200

    update PlayerRepository.java

--- src/main/java/dev/mihail/repository/PlayerRepository.java
+++ src/main/java/dev/mihail/repository/PlayerRepository.java
@@ -4,6 +4,8 @@ import dev.mihail.exceptions.PlayerNotFoundException;
 import dev.mihail.model.Player;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface PlayerRepository extends CrudRepository<Player, Integer> {
@@ -12,13 +14,24 @@ public interface PlayerRepository extends CrudRepository<Player, Integer> {
 
 
     default Player findPlayerByAvatar(String avatar) {
-        String sqlQuery = "SELECT * FROM PLAYER WHERE PLAYER.AVATAR = ?";
+        String sqlQuery = "SELECT * FROM PLAYER WHERE PLAYER.AVATAR = '?'";
             Optional<Player> optionalPlayer = Optional.ofNullable(jdbcTemplate.queryForObject(sqlQuery,
                                                               Player.class, avatar));
             if (optionalPlayer.isEmpty()){
                 throw new PlayerNotFoundException("Player With Avatar" + avatar + "Not Found");
             }
             return jdbcTemplate.queryForObject(sqlQuery, Player.class, avatar);
+    }
+
+    default List<Player> extractAllPlayers() {
+        String sqlQuery = "SELECT * FROM PLAYER";
+        return jdbcTemplate.queryForList(sqlQuery, Player.class);
+    }
+
+    default int extractNumberOfPlayers() {
+        String sqlQuery = "SELECT COUNT (PLAYER.AVATAR) FROM PLAYER";
+        List<Player> playerList = jdbcTemplate.queryForList(sqlQuery, Player.class);
+        return playerList.size();
     }
 
     default int deletePlayerByAvatar(String avatar) {
@@ -30,6 +43,4 @@ public interface PlayerRepository extends CrudRepository<Player, Integer> {
         }
             return jdbcTemplate.update(sqlQuery, avatar);
     }
-
-
 }
