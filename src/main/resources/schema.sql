67f277afeeb4f89bc0da8434c6e0e3e4b0e8dfb2
Mishoto <mihail.dev.io@gmail.com>
2021-11-03 10:39:28 +0200

    update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,13 +3,13 @@ CREATE TABLE `player` (
   `p_age` int(11) NOT NULL DEFAULT 1,
   `p_name` varchar(255) NOT NULL DEFAULT `player`,
   `p_scores` int(11) NOT NULL DEFAULT 0,
-  `p_avatar` varchar(255) NOT NULL DEFAULT `url`
+  `p_avatar` varchar(255) NOT NULL UNIQUE DEFAULT `url`
   PRIMARY KEY (`p_id`)
 );
 
 CREATE TABLE `game` (
   `g_id` int(11) NOT NULL AUTO_INCREMENT,
-  `g_name` varchar(255) NOT NULL DEFAULT `player`,
+  `g_name` varchar(255) NOT NULL UNIQUE DEFAULT `game`,
   PRIMARY KEY (`g_id`)
 );
 
@@ -18,7 +18,7 @@ CREATE TYPE `num_of_players` AS ENUM (`ONE`, `TWO`, `THREE`, `MULTIPLE`);
 
 CREATE TABLE `competition` (
   `c_id` int(11) NOT NULL AUTO_INCREMENT,
-  `c_name` varchar(255) NOT NULL DEFAULT `player`,
+  `c_name` varchar(255) NOT NULL UNIQUE DEFAULT `CN001`,
   `c_difficulty` difficulty(12) NOT NULL DEFAULT `EASY`,
   `c_players` num_of_players(16) NOT NULL DEFAULT `ONE`
   PRIMARY KEY (`c_id`)
