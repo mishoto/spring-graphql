b5faff2ed047160f04e38b5ee98ea884a7f5b9a5
Mishoto <mihail.dev.io@gmail.com>
2021-11-03 15:49:16 +0200

    update PlayerRepository.java

--- src/main/java/dev/mihail/repository/PlayerRepository.java
+++ src/main/java/dev/mihail/repository/PlayerRepository.java
@@ -1,7 +1,35 @@
 package dev.mihail.repository;
 
+import dev.mihail.exceptions.PlayerNotFoundException;
 import dev.mihail.model.Player;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import java.util.Optional;
 
 public interface PlayerRepository extends CrudRepository<Player, Integer> {
+
+    JdbcTemplate jdbcTemplate = new JdbcTemplate();
+
+
+    default Player findPlayerByAvatar(String avatar) {
+        String sqlQuery = "SELECT * FROM PLAYER WHERE PLAYER.AVATAR = ?";
+            Optional<Player> optionalPlayer = Optional.ofNullable(jdbcTemplate.queryForObject(sqlQuery,
+                                                              Player.class, avatar));
+            if (optionalPlayer.isEmpty()){
+                throw new PlayerNotFoundException("Player With Avatar" + avatar + "Not Found");
+            }
+            return jdbcTemplate.queryForObject(sqlQuery, Player.class, avatar);
+    }
+
+    default int deletePlayerByAvatar(String avatar) {
+        String sqlQuery = "DELETE * FROM PLAYER WHERE PLAYER.AVATAR = '?'";
+        Optional<Player> optionalPlayer = Optional.ofNullable(jdbcTemplate.queryForObject(sqlQuery,
+                Player.class, avatar));
+        if (optionalPlayer.isEmpty()){
+            throw new PlayerNotFoundException("Player With Avatar" + avatar + "Not Found");
+        }
+            return jdbcTemplate.update(sqlQuery, avatar);
+    }
+
+
 }
