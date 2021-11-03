6bef63b2a15dabb6c5ceb8a497e3205ff95183d0
Mishoto <mihail.dev.io@gmail.com>
2021-11-03 18:13:18 +0200

    cr & impl PlayerQuery.class

---
+++ src/main/java/dev/mihail/resolver/PlayerQuery.java
@@ -0,0 +1,26 @@
+package dev.mihail.resolver;
+
+import dev.mihail.model.Player;
+import dev.mihail.repository.PlayerRepository;
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class PlayerQuery implements GraphQLQueryResolver {
+
+    private final PlayerRepository playerRepository;
+
+    @Autowired
+    public PlayerQuery(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    public int countPlayers() {
+        return playerRepository.extractNumberOfPlayers();
+    }
+
+    public List<Player> findAllPlayers() {
+        return playerRepository.extractAllPlayers();
+    }
+}
